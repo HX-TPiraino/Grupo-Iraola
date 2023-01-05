04340cc6ea3d915148bc49271560535861841cfb
Tobias <tpiraino@soyhenry.com>
2023-01-05 12:56:04 -0300

    Errores en despliegue

--- src/database/database.js
+++ src/database/database.js
@@ -3,21 +3,8 @@ import dotenv from "dotenv";
 dotenv.config()
 
 
-export const sequelize = process.env.ENTORNO === "Desarrollo" ?
-    new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.HOST || "localhost",
-            dialect: process.env.DIALECT
-        }
-    )
-    :
-    new Sequelize(process.env.DB_DEPLOY,
-        {
-            logging: false,
-            native: false
-        })
-
-
+export const sequelize = new Sequelize(process.env.DB_DEPLOY,
+    {
+        logging: false,
+        native: false
+    })
