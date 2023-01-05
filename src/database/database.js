0bae5222e032a03133849c04090ea18a177a558d
Tobias <tpiraino@soyhenry.com>
2023-01-05 09:38:11 -0300

    Actualizando y agregando variables de entorno

--- src/database/database.js
+++ src/database/database.js
@@ -7,7 +7,7 @@ export const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-    host: process.env.HOST,
+    host: process.env.HOST || "localhost",
     dialect: process.env.DIALECT
     }
 )
