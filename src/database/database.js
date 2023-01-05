1c3b915e2f5ef36bb79749ba3dad8af7e5969183
Tobias <tpiraino@soyhenry.com>
2023-01-05 15:01:03 -0300

    Base de datos variable Local y Deploy

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config()
 
 
-export const sequelize = new Sequelize(process.env.DB_DEPLOY,
+export const sequelize = new Sequelize(process.env.DB_DEPLOY || `${process.env.DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}/${process.env.DB_NAME}`,
 {
     logging: false,
     native: false
