b42e6e32eb1a626fd2c0cd1f9a5ca0efecadef9a
Tobias <tpiraino@soyhenry.com>
2023-01-05 09:32:23 -0300

    Actualizando y agregando variables de entorno

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 export const sequelize = new Sequelize(
     process.env.DB_NAME,
-    process.env.DIALECT,
+    process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
     host: process.env.HOST,
