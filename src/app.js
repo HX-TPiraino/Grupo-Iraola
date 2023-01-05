6a695149e165e9031c25a71c303c8e3f3a3a3194
Tobias <tpiraino@soyhenry.com>
2023-01-05 12:47:53 -0300

    Agregando acceso a nuevas URL

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import personasRoutes from "./routes/index.routes.js"
 const app = express()
 
 app.use(express.json())
+
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin","*")
+})
+
 app.use(personasRoutes)
 
 export default app
--- src/database/database.js
+++ src/database/database.js
@@ -2,12 +2,22 @@ import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config()
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-    host: process.env.HOST || "localhost",
-    dialect: process.env.DIALECT
-    }
-)
+
+export const sequelize = process.env.ENTORNO === "Desarrollo" ?
+    new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.HOST || "localhost",
+            dialect: process.env.DIALECT
+        }
+    )
+    :
+    new Sequelize(process.env.DB_DEPLOY,
+        {
+            logging: false,
+            native: false
+        })
+
+
